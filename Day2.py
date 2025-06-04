d4de581b2f56e0af3cee84e127e781c61b67788e
Muhammed Mansoor C <mdmansoorc3333@gmail.com>
2025-06-04 15:37:57 +0300

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,4 @@
+Length=int(input("Enter Length of the Rectangle: "))
+Width=int(input("Enter Width of the rectangle: "))
+Area=Length*Width
+print(f"The Area of the rectangle for the provided Length of {Length} and width of {Width} is {Area} Square Meters")
